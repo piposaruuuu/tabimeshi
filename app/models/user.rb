2a88c7af29ccf03e8c18cafb5bda31a62a567613
piposaruuuu <87000315+piposaruuuu@users.noreply.github.com>
2021-08-17 11:25:30 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts
+  has_many :comments
+
+  with_options presence: true do
+    validates :nickname
+    validates :password,
+              format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
+  end
 end
