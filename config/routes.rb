eabda77e054f505f4cb8374d9bf3de5a87d7b8bd
piposaruuuu <87000315+piposaruuuu@users.noreply.github.com>
2021-08-19 22:06:11 +0900

    ビューファイルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'posts/index'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "posts#index"
+  resources :posts, only: :index do
+     collection do
+       get 'find'
+     end
+  end
 end
