4dd985b416b6ac50789aa240ed93118d00fb43ef
piposaruuuu <87000315+piposaruuuu@users.noreply.github.com>
2021-08-21 17:15:39 +0900

    Postモデルの再作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
-  
+  has_many  :comments
+  belong_to :user
+  has_one_attached :image
 end
--- db/migrate/20210821062556_create_posts.rb
+++ db/migrate/20210821062556_create_posts.rb
@@ -2,6 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
       t.string     :restaurant_name,   null: false
+      t.string     :meal_name,         null: false
       t.text       :text,              null: false
       t.integer    :price_id,          null: false
       t.integer    :time_period_id,    null: false
