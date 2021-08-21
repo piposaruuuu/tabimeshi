1a346fa9e734827ecc1b91c22723fcab14384cae
piposaruuuu <87000315+piposaruuuu@users.noreply.github.com>
2021-08-21 13:12:09 +0900

    Postモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_055609) do
+ActiveRecord::Schema.define(version: 2021_08_21_032609) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "restaurant_name", null: false
+    t.text "text", null: false
+    t.integer "price_id", null: false
+    t.integer "time_period_id", null: false
+    t.integer "genre_id", null: false
+    t.integer "person_number_id", null: false
+    t.string "country", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +39,5 @@ ActiveRecord::Schema.define(version: 2021_08_16_055609) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
