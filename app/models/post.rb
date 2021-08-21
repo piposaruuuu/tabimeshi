300f166f64aef7f40bc339ee8dcf922177f7096e
piposaruuuu <87000315+piposaruuuu@users.noreply.github.com>
2021-08-21 18:43:42 +0900

    バリデーションの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,9 @@ class Post < ApplicationRecord
   belongs_to :person_number
   belongs_to :price
   belongs_to :time_period
+
+  with_options presence: true do
+    validates :restaurant_name, :meal_name, :text, :country, :image
+    validates :genre_id, :person_number_id, :price_id, :time_period_id, numericality: { other_than: 1, message: "can't be blank" } 
+  end
 end
