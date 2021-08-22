627b63b30baa24594dc21e8204484e72d23116ef
piposaruuuu <87000315+piposaruuuu@users.noreply.github.com>
2021-08-22 17:32:45 +0900

    コントローラーとindexファイルの修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   before_action :move_to_index, except: [:index, :new, :create]
   def index
+    @posts = Post.includes(:user).order('created_at DESC')
   end
 
   def new
